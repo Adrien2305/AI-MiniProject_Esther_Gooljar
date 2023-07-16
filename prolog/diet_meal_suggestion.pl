c4fd58691c7ebaae41a52ec9037cb52ff97214bc
Adrien <alesther@student.udm.ac.mu>
2023-07-16 13:14:14 +0400

    AI Project Esther Gooljar

---
+++ prolog/diet_meal_suggestion.pl
@@ -0,0 +1,197 @@
+% Diet Meal Suggestion Expert System
+
+% Define the meal suggestions based on dietary preferences
+suggest_meal(vegan, 'Vegan Burger with Sweet Potato Fries').
+suggest_meal(vegan, 'Chickpea Curry with Brown Rice').
+suggest_meal(vegan, 'Vegan Pad Thai with Tofu').
+suggest_meal(vegan, 'Roasted Vegetable Buddha Bowl').
+suggest_meal(vegan, 'Vegan Lentil Soup').
+suggest_meal(vegan, 'Chickpea Salad Wraps').
+suggest_meal(vegan, 'Quinoa Stuffed Bell Peppers').
+suggest_meal(vegan, 'Vegan Chili with Cornbread').
+suggest_meal(vegan, 'Sweet Potato and Black Bean Enchiladas').
+suggest_meal(vegan, 'Vegan Caesar Salad with Tempeh Bacon').
+suggest_meal(vegan, 'Spaghetti Aglio e Olio with Roasted Vegetables').
+suggest_meal(vegan, 'Vegan Sushi Rolls with Avocado and Cucumber').
+suggest_meal(vegan, 'Moroccan Chickpea and Vegetable Tagine').
+suggest_meal(vegan, 'Vegan Mushroom Risotto').
+suggest_meal(vegan, 'Veggie Stir-Fry with Peanut Sauce').
+suggest_meal(vegan, 'Vegan BBQ Jackfruit Sandwiches').
+suggest_meal(vegan, 'Mediterranean Quinoa Salad').
+suggest_meal(vegan, 'Vegan Mexican Street Corn').
+suggest_meal(vegan, 'Thai Red Curry with Tofu and Vegetables').
+suggest_meal(vegan, 'Vegan Spinach and Artichoke Stuffed Shells').
+suggest_meal(vegan, 'Coconut Curry Lentil Soup').
+suggest_meal(vegan, 'Vegan Baked Ziti with Cashew Cheese').
+
+
+
+
+% Vegetarian meal
+suggest_meal(vegetarian, 'Vegetarian Tacos with Guacamole').
+suggest_meal(vegetarian, 'Eggplant Parmesan with Spaghetti').
+suggest_meal(vegetarian, 'Spinach and Mushroom Quesadillas').
+suggest_meal(vegetarian, 'Vegetable Stir-Fry with Rice').
+suggest_meal(vegetarian, 'Caprese Salad with Balsamic Glaze').
+suggest_meal(vegetarian, 'Mushroom and Lentil Shepherd\'s Pie').
+suggest_meal(vegetarian, 'Cauliflower Curry with Naan Bread').
+suggest_meal(vegetarian, 'Roasted Vegetable Fajitas').
+suggest_meal(vegetarian, 'Vegetable Paella').
+suggest_meal(vegetarian, 'Quinoa Stuffed Zucchini Boats').
+suggest_meal(vegetarian, 'Vegetarian Bibimbap').
+suggest_meal(vegetarian, 'Vegetable Lasagna').
+suggest_meal(vegetarian, 'Mediterranean Chickpea Salad').
+suggest_meal(vegetarian, 'Portobello Mushroom Burger with Sweet Potato Fries').
+suggest_meal(vegetarian, 'Vegetable Pad Thai').
+suggest_meal(vegetarian, 'Sweet Potato and Black Bean Quesadillas').
+suggest_meal(vegetarian, 'Broccoli and Cheese Stuffed Potatoes').
+suggest_meal(vegetarian, 'Vegetarian Sushi Rolls').
+suggest_meal(vegetarian, 'Vegetable Curry with Basmati Rice').
+
+
+suggest_meal(gluten_free, 'Gluten-Free Pasta with Tomato Sauce').
+suggest_meal(gluten_free, 'Quinoa Stuffed Bell Peppers').
+suggest_meal(gluten_free, 'Grilled Salmon with Roasted Vegetables').
+suggest_meal(gluten_free, 'Zucchini Noodles with Pesto Sauce').
+suggest_meal(gluten_free, 'Baked Chicken Breast with Steamed Broccoli').
+suggest_meal(gluten_free, 'Cauliflower Fried Rice').
+suggest_meal(gluten_free, 'Grilled Shrimp Skewers with Quinoa Salad').
+suggest_meal(gluten_free, 'Baked Tilapia with Lemon Garlic Sauce').
+suggest_meal(gluten_free, 'Stuffed Portobello Mushrooms with Spinach and Feta').
+suggest_meal(gluten_free, 'Roasted Chicken Thighs with Sweet Potato Mash').
+suggest_meal(gluten_free, 'Taco Lettuce Wraps with Ground Turkey').
+suggest_meal(gluten_free, 'Baked Cod with Herbed Quinoa').
+suggest_meal(gluten_free, 'Grilled Steak with Chimichurri Sauce').
+suggest_meal(gluten_free, 'Lemon Herb Roasted Chicken with Roasted Vegetables').
+suggest_meal(gluten_free, 'Shrimp and Vegetable Stir-Fry with Brown Rice').
+
+
+suggest_meal(dairy_free, 'Stir-Fried Tofu with Vegetables').
+suggest_meal(dairy_free, 'Coconut Curry with Basmati Rice').
+suggest_meal(dairy_free, 'Lentil Soup with Roasted Vegetables').
+suggest_meal(dairy_free, 'Chickpea Salad with Lemon Vinaigrette').
+suggest_meal(dairy_free, 'Quinoa and Black Bean Tacos with Salsa').
+suggest_meal(dairy_free, 'Grilled Vegetable Skewers with Balsamic Glaze').
+suggest_meal(dairy_free, 'Cauliflower Rice Stir-Fry with Cashews').
+suggest_meal(dairy_free, 'Roasted Eggplant and Tomato Pasta').
+suggest_meal(dairy_free, 'Vegan Coconut Curry with Tofu').
+suggest_meal(dairy_free, 'Vegetable Stir-Fry with Tamari Sauce').
+suggest_meal(dairy_free, 'Mushroom Risotto with Truffle Oil').
+suggest_meal(dairy_free, 'Spaghetti Squash with Marinara Sauce').
+suggest_meal(dairy_free, 'Sweet Potato and Black Bean Chili').
+suggest_meal(dairy_free, 'Quinoa Salad with Roasted Vegetables').
+suggest_meal(dairy_free, 'Crispy Baked Falafel with Hummus and Salad').
+
+suggest_meal(low_carb, 'Grilled Chicken with Steamed Broccoli').
+suggest_meal(low_carb, 'Zucchini Noodles with Pesto').
+suggest_meal(low_carb, 'Salmon with Asparagus and Lemon Butter Sauce').
+suggest_meal(low_carb, 'Cauliflower Fried Rice with Shrimp').
+suggest_meal(low_carb, 'Eggplant Lasagna with Ground Turkey').
+suggest_meal(low_carb, 'Baked Cod with Roasted Vegetables').
+suggest_meal(low_carb, 'Greek Salad with Grilled Shrimp').
+suggest_meal(low_carb, 'Stuffed Bell Peppers with Ground Beef and Cheese').
+suggest_meal(low_carb, 'Grilled Steak with Garlic Butter Mushrooms').
+suggest_meal(low_carb, 'Chicken Caesar Salad with Avocado').
+
+
+suggest_meal(low_calorie, 'Mixed Green Salad with Grilled Shrimp').
+suggest_meal(low_calorie, 'Cauliflower Fried Rice').
+suggest_meal(low_calorie, 'Vegetable Soup with Quinoa').
+suggest_meal(low_calorie, 'Grilled Chicken Breast with Steamed Vegetables').
+suggest_meal(low_calorie, 'Baked Salmon with Roasted Asparagus').
+suggest_meal(low_calorie, 'Zucchini Noodles with Marinara Sauce').
+suggest_meal(low_calorie, 'Spinach and Mushroom Omelette').
+suggest_meal(low_calorie, 'Turkey Lettuce Wraps').
+suggest_meal(low_calorie, 'Broiled Cod with Lemon and Herbs').
+suggest_meal(low_calorie, 'Stir-Fried Tofu with Broccoli').
+suggest_meal(low_calorie, 'Quinoa Salad with Fresh Vegetables').
+suggest_meal(low_calorie, 'Roasted Brussels Sprouts and Chickpea Salad').
+suggest_meal(low_calorie, 'Baked Eggplant with Tomato and Basil').
+suggest_meal(low_calorie, 'Shrimp and Avocado Ceviche').
+suggest_meal(low_calorie, 'Grilled Vegetable Skewers with Tofu').
+
+
+suggest_meal(omnivore, 'Grilled Steak with Roasted Potatoes').
+suggest_meal(omnivore, 'Teriyaki Chicken with Jasmine Rice').
+suggest_meal(omnivore, 'Baked Salmon with Lemon Butter Sauce').
+suggest_meal(omnivore, 'Spaghetti Bolognese with Garlic Bread').
+suggest_meal(omnivore, 'Honey Glazed Roasted Chicken with Mashed Sweet Potatoes').
+suggest_meal(omnivore, 'Beef Stir-Fry with Vegetables').
+suggest_meal(omnivore, 'Crispy Fried Chicken with Coleslaw').
+suggest_meal(omnivore, 'Pan-Seared Scallops with Risotto').
+suggest_meal(omnivore, 'Pork Tenderloin with Apple Chutney').
+suggest_meal(omnivore, 'Shrimp Tacos with Avocado Lime Sauce').
+suggest_meal(omnivore, 'Lemon Herb Roasted Turkey with Green Beans').
+suggest_meal(omnivore, 'Sesame Ginger Glazed Salmon with Steamed Broccoli').
+suggest_meal(omnivore, 'Pesto Pasta with Grilled Chicken').
+suggest_meal(omnivore, 'Lamb Chops with Mint Sauce and Roasted Vegetables').
+suggest_meal(omnivore, 'Creamy Tuscan Chicken with Sun-Dried Tomatoes').
+suggest_meal(omnivore, 'Baked Cod with Herbed Couscous').
+suggest_meal(omnivore, 'Bacon-Wrapped Stuffed Pork Tenderloin').
+suggest_meal(omnivore, 'Honey Mustard Glazed Ham with Roasted Potatoes').
+
+
+
+% Web interface for diet meal suggestion
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_parameters)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/json)).
+
+% Define the HTTP handler for the meal suggestion
+:- http_handler(root(suggest_meal), suggest_meal_handler, []).
+
+% Main HTTP handler
+:- http_handler(root(.), home_handler, []).
+
+% Home page handler
+home_handler(_Request) :-
+    reply_html_page(
+        title('Diet Meal Suggestion'),
+        [ h1('Diet Meal Suggestion'),
+          p('Select your dietary preference:'),
+          form([action='/suggest_meal', method='POST'],
+               [ select(name='preference',
+                        [ option([value=vegan], 'Vegan'),
+                          option([value=vegetarian], 'Vegetarian'),
+                          option([value=pescatarian], 'Pescatarian'),
+                          option([value=gluten_free], 'Gluten-Free'),
+                          option([value=dairy_free], 'Dairy-Free'),
+                          option([value=low_carb], 'Low Carb'),
+                          option([value=low_calorie], 'Low Calorie'),
+                          option([value=omnivore], 'Omnivore')
+                        ]),
+                 br([]),
+                 input([type='submit', value='Get Meal Suggestion'])
+               ])
+        ]).
+
+% Meal suggestion handler
+suggest_meal_handler(Request) :-
+    http_parameters(Request, [preference(Preference, [])]),
+    findall(Meal, suggest_meal(Preference, Meal), Meals),
+    reply_html_page(
+        title('Diet Meal Suggestion'),
+        [ h1('Diet Meal Suggestion'),
+          p('Based on your preference, we suggest the following meals:'),
+          \meals_table(Meals)
+        ]).
+
+% Helper predicate to generate the meals table
+meals_table(Meals) -->
+    html(table([ \meals_header | \meals_rows(Meals) ])).
+
+% Helper predicate to generate the meals table header
+meals_header -->
+    html(tr(th('Meal'))).
+
+% Helper predicate to generate the meals table rows
+meals_rows([]) --> [].
+meals_rows([Meal | Rest]) -->
+    html(tr(td(Meal))),
+    meals_rows(Rest).
+
+% Start the server on port 8000
+:- initialization(http_server(http_dispatch, [port(8000)])).
---
+++ prolog/suggest_meal.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Diet Meal Suggestion</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        form {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        label {
+            font-weight: bold;
+        }
+
+        select {
+            margin-top: 5px;
+            padding: 5px;
+        }
+
+        input[type="submit"] {
+            margin-top: 10px;
+            padding: 8px 16px;
+            font-weight: bold;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .meal-suggestion {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Diet Meal Suggestion</h1>
+
+    <form action="suggest_meal.php" method="POST">
+        <label for="preference">Select your dietary preference:</label>
+        <br>
+        <select name="preference" id="preference">
+            <option value="vegan">Vegan</option>
+            <option value="vegetarian">Vegetarian</option>
+            
+            <option value="gluten_free">Gluten-Free</option>
+            <option value="dairy_free">Dairy-Free</option>
+            <option value="low_carb">Low Carb</option>
+            <option value="low_calorie">Low Calorie</option>
+            <option value="omnivore">Omnivore</option>
+        </select>
+        <br>
+        <input type="submit" value="Get Meal Suggestion">
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $preference = $_POST['preference'];
+        $command = 'swipl -s C:/xampp/htdocs/prolog/diet_meal_suggestion.pl -g "findall(Meal, suggest_meal(\'' . $preference . '\', Meal), Meals), atomic_list_concat(Meals, \',\', Output), writeln(Output), halt."';
+        $output = shell_exec($command);
+        
+        $meals = explode(",", trim($output));
+        
+        if (!empty($meals)) {
+            echo '<div class="meal-suggestion">';
+            echo '<h3>Based on your preference, we suggest the following meals:</h3>';
+            echo '<table>';
+            echo '<tr><th>Meal</th></tr>';
+            foreach ($meals as $meal) {
+                echo '<tr><td>' . trim($meal) . '</td></tr>';
+            }
+            echo '</table>';
+            echo '</div>';
+        } else {
+            echo '<div class="meal-suggestion">';
+            echo '<p>No meal suggestions found for your preference.</p>';
+            echo '</div>';
+        }
+    }
+    ?>
+</body>
+</html>
